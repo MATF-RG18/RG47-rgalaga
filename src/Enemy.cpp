249b89020c1b640e23a277167978f915eac67a4f
cane <canelp94@gmail.com>
2019-08-20 02:12:48 +0200

    Enemy class added

---
+++ src/Enemy.cpp
@@ -0,0 +1,22 @@
+#include "Enemy.h"
+
+#include "glm/glm.hpp"
+#include "glm/gtc/matrix_transform.hpp"
+
+
+Enemy::Enemy(float x, float y, float z, std::string texName,
+               float angle, float width, float height, float step)
+    : Spacecraft(x, y, z, texName, angle, width, height, step)
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::Move(const float x, const float y, const float z, const float angle)
+{
+
+}
---
+++ src/headers/Enemy.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Debug.h"
+#include "Spacecraft.h"
+
+
+class Enemy : public Spacecraft
+{
+private:
+    float m_step;
+
+public:
+    Enemy(float x, float y, float z, std::string texName,
+          float angle, float width, float height, float step);
+    virtual ~Enemy();
+
+    void Move(const float x, const float y, const float z, const float angle);
+};
